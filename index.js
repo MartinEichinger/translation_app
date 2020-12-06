112c74033928bcf2a48f8241e84627c9d9f30666
EdMadd <martl.eichinger@gmail.com>
2020-12-06 08:46:20 +0100

    Update

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ async function translate() {
     var outputLanguage = document.querySelector('#outputLanguage').value
 
     console.log("Input: ", inputText);
-    //var url = 'http://localhost:8010/proxy/translate_a/single?client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&hl=%25s&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&iid=1dd3b944-fa62-4b55-b330-74909a99969e&';
     var url = 'https://cors-anywhere.herokuapp.com/https://translate.google.com/translate_a/single?client=at&dt=t&dt=ld&dt=qca&dt=rm&dt=bd&dj=1&hl=%25s&ie=UTF-8&oe=UTF-8&inputm=2&otf=2&iid=1dd3b944-fa62-4b55-b330-74909a99969e&';
     var data = {'sl': selectLanguage(inputLanguage), 'tl': selectLanguage(outputLanguage), 'q': inputText};
     var res = await postData(url, data);
